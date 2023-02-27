6ddda5ddb4cfd5d13c917fa7b2ff9e7ea37e3f1e
sean-code <john.nganga@student.moringaschool.com>
2023-02-27 20:34:49 +0300

    Update driver section

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import "../../styles/about-section.css";
-import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
 
 const AboutSection = ({ aboutClass }) => {
   return (
@@ -18,7 +17,7 @@ const AboutSection = ({ aboutClass }) => {
           <Col lg="6" md="6">
             <div className="about__section-content">
               <h4 className="section__subtitle">About Us</h4>
-              <h2 className="section__title">Welcome to car rent service</h2>
+              <h2 className="section__title">Welcome to Car Rental</h2>
               <p className="section__description">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Voluptatum blanditiis esse accusantium dignissimos labore
@@ -56,7 +55,7 @@ const AboutSection = ({ aboutClass }) => {
 
           <Col lg="6" md="6">
             <div className="about__img">
-              <img src={aboutImg} alt="" className="w-100" />
+              <img src='https://media.istockphoto.com/id/494245526/photo/generic-black-sport-coupe-car.jpg?s=612x612&w=0&k=20&c=yCasxIiwKLPRpEPSp2i7LjExJmSB7U6ZTrt1856h7bU=' alt="" className="w-100" />
             </div>
           </Col>
         </Row>
--- src/components/UI/BecomeDriverSection.jsx
+++ src/components/UI/BecomeDriverSection.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import "../../styles/become-driver.css";
 import { Container, Row, Col } from "reactstrap";
 
-import driverImg from "../../assets/all-images/toyota-offer-2.png";
 
 const BecomeDriverSection = () => {
   return (
     <section className="become__driver">
       <Container>
         <Row>
-          <Col lg="6" md="6" sm="12" className="become__driver-img">
-            <img src={driverImg} alt="" className="w-100" />
-          </Col>
+          {/* <Col lg="6" md="6" sm="12" className="become__driver-img">
+            <img src='' alt="" className="w-100" />
+          </Col> */}
 
           <Col lg="6" md="6" sm="12">
             <h2 className="section__title become__driver-title">
