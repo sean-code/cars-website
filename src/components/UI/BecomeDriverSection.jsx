ed31735b488901934b6d756e8cf9a0d60ca3b993
sean-code <john.nganga@student.moringaschool.com>
2023-02-27 20:39:09 +0300

    Update padding on buttons

--- src/components/UI/BecomeDriverSection.jsx
+++ src/components/UI/BecomeDriverSection.jsx
@@ -14,7 +14,7 @@ const BecomeDriverSection = () => {
 
           <Col lg="6" md="6" sm="12">
             <h2 className="section__title become__driver-title">
-              Do You Want to Earn With Us? So Don't Be Late
+              Want to Earn With Us?
             </h2>
 
             <button className="btn become__driver-btn mt-4">
