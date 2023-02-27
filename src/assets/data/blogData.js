a6941c8aee83584aa1fe8ffefca72b0a92a16242
sean-code <john.nganga@student.moringaschool.com>
2023-02-27 23:57:53 +0300

    Riddance of smelling code

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -1,7 +1,3 @@
-// import images from all-images/blog-img directory
-import img01 from "../all-images/blog-img/blog-1.jpg";
-import img02 from "../all-images/blog-img/blog-2.jpg";
-import img03 from "../all-images/blog-img/blog-3.jpg";
 
 const blogData = [
   {
